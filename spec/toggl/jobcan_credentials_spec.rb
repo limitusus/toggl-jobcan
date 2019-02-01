a3c44920c90903e19a76f4361267955c63b449f6
Tomoya Kabe <limit.usus@gmail.com>
2019-02-02 00:07:43 +0900

    Add minimum tests

---
+++ spec/toggl/jobcan_credentials_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe Toggl::Jobcan::Credentials do
+  let(:config) { 'spec/testdata/sample_config.yaml' }
+
+ it 'can be initialized' do
+   o = Toggl::Jobcan::Credentials.new({path: config})
+   expect(o).to be_a Toggl::Jobcan::Credentials
+ end
+
+ it 'can load config' do
+   o = Toggl::Jobcan::Credentials.new({path: config})
+   expect(o.email).to eq 'alice@example.com'
+   expect(o.password).to eq 'pa55w0rd'
+  end
+end
